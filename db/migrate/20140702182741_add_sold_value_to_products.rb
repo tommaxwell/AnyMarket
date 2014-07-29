dd925f395198be181be09aa08fe5d211e0259135
Tom Maxwell <tommaxwell95@gmail.com>
2014-07-29 20:40:45 +0000

    f

--- db/migrate/20140702182741_add_sold_value_to_products.rb
+++ db/migrate/20140702182741_add_sold_value_to_products.rb
@@ -1,5 +1,5 @@
 class AddSoldValueToProducts < ActiveRecord::Migration
   def change
-    add_column :products, :sold_value, :boolean, :default => 2
+    add_column :products, :sold_value, :boolean, :default => 0
   end
 end
