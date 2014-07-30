10d29695e4c9ab3935833da3c6b1229a67c72b81
Tom Maxwell <tommaxwell95@gmail.com>
2014-07-30 06:54:07 +0000

    pricing formatting

--- db/migrate/20140709010333_add_decimal_price_to_products.rb
+++ db/migrate/20140709010333_add_decimal_price_to_products.rb
@@ -1,5 +1,5 @@
 class AddDecimalPriceToProducts < ActiveRecord::Migration
   def change
-    add_column :products, :decimal_price, :decimal, :precision => 8, :scale => 2
+    add_column :products, :decimal_price, :decimal, :precision => 8, :scale => 3
   end
 end
