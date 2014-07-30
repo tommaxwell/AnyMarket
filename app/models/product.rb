acf8a3992481947753a37b24c503347f29de5660
Tom Maxwell <tommaxwell95@gmail.com>
2014-07-30 02:36:51 +0000

    imagez

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,8 @@ class Product < ActiveRecord::Base
   belongs_to :user
   belongs_to :category, :foreign_key => :category_id
   attr_accessor :destroy_photo
-  has_attached_file :photo, :styles => {:medium => "400x400>", :thumb => "100x100>"}
+	has_attached_file :photo, :styles => {:medium => "400x400>", :thumb => "100x100>"},:storage => :s3, :s3_credentials => {:bucket => ENV['S3_BUCKET_NAME'],:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}, :path => "anymarket/:attachment/:style/:id.:extension"
+		
   validates_attachment :photo, :content_type => { :content_type => ["image/jpeg","image/png"]}
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
   
