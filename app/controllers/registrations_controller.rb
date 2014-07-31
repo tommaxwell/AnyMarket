6501e3035c2256a8fd2bfd1cc69cbf2ee7470d97
Tom Maxwell <tommaxwell95@gmail.com>
2014-07-31 21:39:54 +0000

    Ds

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,7 +11,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
   
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar, :school)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar, :school, :provider, :uid)
   end
   
   def account_update_params 
