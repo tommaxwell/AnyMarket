a15fa7388324f1b0ab7384abfb41c1c4421eb461
Tom Maxwell <tommaxwell95@gmail.com>
2014-07-31 21:31:52 +0000

    FB

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,24 @@ class User < ActiveRecord::Base
   def mailboxer_email(object)
     email
   end
+	
+	def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => auth.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(name:auth.extra.raw_info.name,
+                            provider:auth.provider,
+                            uid:auth.uid,
+                            email:auth.info.email,
+                            password:Devise.friendly_token[0,20],
+                          )
+      end    end
+  end
   
   
   has_many :products, -> { order "created_at DESC" }
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -253,4 +253,6 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+	require "omniauth-facebook"
+	config.omniauth :facebook, "562365530534757", "4fbca52392d227bb64b3a9c4d236eb51"
 end
