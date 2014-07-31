980313de6488a4815169c9c24450f976eec41d9a
Tom Maxwell <tommaxwell95@gmail.com>
2014-07-31 21:44:38 +0000

    Ds

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-	:recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
+	:recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:facebook]
   
   FIELDS = [:phone, :website, :company, :fax, :addresses, :credit_cards]
   
@@ -25,23 +25,14 @@ class User < ActiveRecord::Base
     email
   end
 	
-	def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    if user
-      return user
-    else
-      registered_user = User.where(:email => auth.info.email).first
-      if registered_user
-        return registered_user
-      else
-        user = User.create(name:auth.extra.raw_info.name,
-                            provider:auth.provider,
-                            uid:auth.uid,
-                            email:auth.info.email,
-                            password:Devise.friendly_token[0,20],
-                          )
-      end    end
+	def self.from_omniauth(auth)
+  where(auth.slice(:provider, :uid)).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    user.name = auth.info.name   # assuming the user model has a name
+    user.image = auth.info.image # assuming the user model has an image
   end
+end
   
   
   has_many :products, -> { order "created_at DESC" }
