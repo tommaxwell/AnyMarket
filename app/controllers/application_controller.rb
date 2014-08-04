e51c3f1e897b2b4346b33046b249586004d2c622
Tom Maxwell <tommaxwell95@gmail.com>
2014-08-04 03:19:12 +0000

    DS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-	before_filter :store_locaton
-	# might need to be before_filter? 
 	
-	def store_location 
-		session[:previous_url] = request.fullpath unless request.fullpath =~ /\\users/
-	end
-	
-	def after_sign_in_path_for(resource)
-		session[:previous_url] || root_path
-	end
   
   def user
     @user = current_user
