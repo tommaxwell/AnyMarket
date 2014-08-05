d76fbd589499a788494011f2747943019cb98e62
Tom Maxwell <tommaxwell95@gmail.com>
2014-08-05 03:53:41 +0000

    fart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,12 @@ class HomeController < ApplicationController
   def index
     @user = current_user
   end
+	
 	def new 
 		respond_to do |format|
 			format.js
 			format.html
 		end
 	end
+	
 end
