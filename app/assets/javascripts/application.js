2094a04ef983c2cc1c6f14eedb7b3b56e52a48dc
Tom Maxwell <tommaxwell95@gmail.com>
2014-08-07 08:13:55 +0000

    DS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
-//= require_tree .
-//= require jquery
 //= require jquery.validate
+//= require bootstrap-dropdown
 //= require bootstrap-sprockets
 //= require bootstrap
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
