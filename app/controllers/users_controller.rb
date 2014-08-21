be351dd34e6bc2db004e0bc36de1612c799074a4
Tom Maxwell <tommaxwell95@gmail.com>
2014-08-21 20:18:19 +0000

    Dsd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
   def profile
-		@user = User.find(current_user.id)
+    @user = User.find(params[:id])
     respond_to do |format|
       format.html
     end
