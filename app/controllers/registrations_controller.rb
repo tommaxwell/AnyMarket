e8eac7e008eb14f5eaf4af0ad44784462537b459
Tom Maxwell <tommaxwell95@gmail.com>
2014-08-31 05:02:35 +0000

    dsd

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,8 +11,8 @@ class RegistrationsController < Devise::RegistrationsController
   
   def after_inactive_sign_up_path_for(user)
 		respond_to do |format|
-			format.html {redirect_to root_url }
-			format.js { render action: "/home/onboarding_modal.js.erb" }
+			format.html {redirect_to root_url}
+			format.js { render :template => "/home/onboarding_modal.js.erb" }
 		end
   end
   
