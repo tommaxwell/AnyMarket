db75c11e5bad5fd20c9fd5a0d3e9ea99e6adf5f6
Tom Maxwell <tommaxwell95@gmail.com>
2014-09-04 05:23:34 +0000

    Dsd

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,10 +8,10 @@ class ProductsController < ApplicationController
       if @product.save
 				if !current_user.braintree_customer_id?
 					flash.now[:addmethod] = ""
-					format.html {redirect_to "show"}
+					redirect_to "show"
 				else
 					flash.now[:notice] = "Your item is for sale!"
-					format.html {redirect_to "show"}
+					redirect_to "show"
 				end
       else
         flash.now[:alert] = "Woops, looks like something went wrong."
