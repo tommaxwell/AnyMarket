d794d74bf8f68694d1513417999ebc9d43baff27
Tom Maxwell <tommaxwell95@gmail.com>
2014-09-04 06:05:52 +0000

    Dsd

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
     respond_to do |format|
       if @product.save
 				if !current_user.braintree_customer_id?
-					flash.now[:addmethod] 
+					flash.now[:addmethod] = ""
 					redirect_to view_item_path(@product.id)
 				else
 					format.html {render :action => "show"}
