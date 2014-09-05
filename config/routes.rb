a859d195cb13f01b24f83b4c8070917dfcac154d
Tom Maxwell <tommaxwell95@gmail.com>
2014-09-05 04:50:21 +0000

    Dsd'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
+	devise_for :users, :controllers => { registrations: 'registrations' }
 	default_url_options :host => "anymarket.herokuapp.com"
 
   root "home#index" 
