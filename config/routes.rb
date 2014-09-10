0ffd77c40c61f7d1f7fca3223144221bf2bb90ea
Tom Maxwell <tommaxwell95@gmail.com>
2014-09-10 09:05:30 +0000

    Dsd

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root "home#index" 
 	
-	get "/users/confirmation/:confirmation_token(.:format)" => "#confirmations#show", :as => "user_confirmation"
+	get "/users/confirmation(.:format)" => "#confirmations#show", :as => "user_confirmation"
 	
 	get "/terms(.:format)" => "home#terms"
 	get "/about(.:format)" => "home#about"
