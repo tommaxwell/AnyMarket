a123838a8c57343caf1410dc52d099995a6ee7f5
Tom Maxwell <tommaxwell95@gmail.com>
2014-09-10 05:27:19 +0000

    Dsd

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-	:recoverable, :rememberable, :trackable, :validatable, :token_validatable, :confirmable
+	:recoverable, :rememberable, :trackable, :validatable, :confirmable
   
   FIELDS = [:phone, :website, :company, :fax, :addresses, :credit_cards]
   
