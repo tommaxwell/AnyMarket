23102f1fd73f6fa685d4eeafe6a912851e7e3f62
Tom Maxwell <tommaxwell95@gmail.com>
2014-09-10 06:33:28 +0000

    Dsd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 	
-	config.action_mailer.default_url_options = { :host => 'anymarket.co'} 
+	config.mandrill_mailer.default_url_options = { :host => 'anymarket.co'} 
 	
 	config.paperclip_defaults = {
 		:storage => :s3,
