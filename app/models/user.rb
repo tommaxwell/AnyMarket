0edc55d1626144b282f5a63b2308a6b442c1d591
Tom Maxwell <tommaxwell95@gmail.com>
2014-09-10 06:02:40 +0000

    Dsd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	after_validation :set_school
+	#after_validation :set_school
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
